87144d7d6694b01a3d33abb291177991fd470167
yahya shawky <yahyashaeky@gmail.com>
2026-07-15 05:15:54 +0300

    feat: create reusable product price component

--- src/components/product/ProductPrice.jsx
+++ src/components/product/ProductPrice.jsx
@@ -1,9 +1,18 @@
-function ProductPrice() {
-    return (
-        <div>
-            
-        </div>
-    )
+import { formatCurrency } from "../../utils/helper";
+function ProductPrice({ price, compareAtPrice }) {
+  return (
+    <div className="flex flex-col items-end">
+      {compareAtPrice && (
+        <span className="text-sm text-gray-400 line-through">
+          {formatCurrency(compareAtPrice)}
+        </span>
+      )}
+
+      <span className="text-xl font-bold text-gray-900">
+        {formatCurrency(price)}
+      </span>
+    </div>
+  );
 }
 
-export default ProductPrice
+export default ProductPrice;
